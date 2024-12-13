6789d5e9a6422d0e67cae0c540dc3fb4cf3e5f19
Jen-Heard <jlheard11@gmail.com>
2024-12-13 11:53:52 -0600

    Completed all steps to the exercise

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
 const topicElement = document.querySelector(".topic");
 const countElement = document.querySelector(".count");
+
+const learning = {
+    topic: "JS",
+    learningGoals: ["complete JS basics course", "go through all material a second time", "complete JS projects", "complete JS React course"],
+    category: "Front End Developer",
+    topicImportance: "High",
+    }; 
+    console.log(learning);
+learning.topic = "JavaScript";
+learning.learningGoals.splice(1, 1);
+console.log(learning);
+
+topicElement.innerText = `I'm learning ${learning.topic}.`;
+topicElement.classList.remove("hide");
+
+countElement.innerText = `I have ${learning.learningGoals.length} learning goals.`;
+countElement.classList.remove("hide");
